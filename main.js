81fbc394763cff8283b72f1102c34677a32a3e47
daniel_cohen <daniel1cohen13579@gmail.com>
2023-09-26 12:18:30 +0300

    update ui

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-var sumClicked_home = 0;
-var sumClicked_summary = 0;
-var sumClicked_about = 0;
-var sumClicked_contact = 0;
-
 function home_open(){
-    sumClicked_home += 1;
-    console.log('home clicked: ' + sumClicked_home)
+    console.log('home clicked !')
 }
 function summary_open(){
-    sumClicked_summary += 1;
-    console.log('summary clicked: ' + sumClicked_summary)
+    console.log('summary clicked !')
     const summarySection = document.getElementById("summary");
     summarySection.scrollIntoView({ behavior: "smooth" });
 }
 function about_open(){
-    sumClicked_about += 1;
-    console.log('about clicked: ' + sumClicked_about)
+    console.log('about clicked !')
     const aboutSection = document.getElementById("about");
     aboutSection.scrollIntoView({ behavior: "smooth" });
 }
 function contact_open(){
-    sumClicked_contact += 1;
-    console.log('contact clicked: ' + sumClicked_contact)
+    console.log('contact clicked !')
+    const contactSection = document.getElementById("contact");
+    contactSection.scrollIntoView({ behavior: "smooth" });
+    
 }
