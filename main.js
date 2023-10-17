9ce2b67a9040905c3ada086c8887a3b55fd1ddcd
daniel_cohen <daniel1cohen13579@gmail.com>
2023-10-17 15:57:26 +0300

    adding a popup login

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ const observer = new IntersectionObserver((entries)=>{
 
 const hiddenElement = document.querySelectorAll('.hidden');
 hiddenElement.forEach((el)=> observer.observe(el));
+
+document.getElementById("show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+    document.querySelector(".article").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+    document.querySelector(".article").classList.remove("active");
+});
