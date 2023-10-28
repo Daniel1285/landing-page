188b7648259ea86c9df2ab7cd4df43319df4582a
daniel_cohen <daniel1cohen13579@gmail.com>
2023-10-28 22:21:12 +0300

    add summary

--- main.js
+++ main.js
@@ -15,8 +15,38 @@ hiddenElement.forEach((el)=> observer.observe(el));
 document.getElementById("show-login").addEventListener("click", function(){
     document.querySelector(".popup").classList.add("active");
     document.querySelector(".article").classList.add("active");
-});
+});    
 document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    closePopup();
+});
+
+if (typeof(Storage) !== "undefined") {
+    console.log("storage available")
+  } else {
+    console.log("Sorry! No Web Storage support..") 
+  }
+
+
+document.getElementById("sign").addEventListener("click", function(){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    if(email && password){
+        document.getElementById("email").value= "";
+        document.getElementById("password").value= "";
+        document.getElementById("show-login").style.opacity = "0";
+        closePopup();
+    } else{
+        console.log("fields are empty!");
+    }
+/*
+    localStorage.setItem("userEmail", email.value);
+    localStorage.setItem("userPassword", password.value);
+*/
+});
+
+
+
+function closePopup(){
     document.querySelector(".popup").classList.remove("active");
     document.querySelector(".article").classList.remove("active");
-});
+}
